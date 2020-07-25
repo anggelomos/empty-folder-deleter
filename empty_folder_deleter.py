45b0b00e263c9fdcc362ca4eaadc125e8f8473a7
Angelo Mosquera <anggelomos@outlook.com>
2020-07-25 09:38:38 -0500

    mend

--- empty_folder_deleter.py
+++ empty_folder_deleter.py
@@ -1,7 +1,12 @@
 import os
+import sys
 
+try:
+    path = sys.argv[1]
+except IndexError:
+    path = "."
 
-def folder_deleter(folder_name: str, verbose_mode: bool = True) -> int:
+def folder_deleter(folder_name: str, verbose_mode: bool = True) -> bool:
     """Delete a folder if it is empty, if the folder is deleted successfully it returns 0 otherwise returns 1
 
     Parameters
@@ -16,7 +21,7 @@ def folder_deleter(folder_name: str, verbose_mode: bool = True) -> int:
     successfull_delete : bool
         If the folder is deleted successfully it returns 0 otherwise returns 1
     """
-
+    
     successfull_delete = 1
     if os.listdir(folder_name) == []:
         os.rmdir(folder_name)
